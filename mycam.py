93df80a2c855015a19389cc853e07bc61ca1bfd2
an-other <72676408+an-other@users.noreply.github.com>
2020-11-30 09:02:01 +0800

    Update mycam.py

--- mycam.py
+++ mycam.py
@@ -7,15 +7,13 @@ import numpy as np
 
 def returncam(weight,features):
 	
-	cam=weight[class_index].unsqueeze(1).unsqueeze(2)*features.squeeze(0) #c*h*w
+	cam=weight.unsqueeze(1).unsqueeze(2)*features.squeeze(0) #c*h*w
 	cam=torch.sum(cam,dim=0)  #h*w
 	assert(len(cam.shape)==2),'Error:wrong shape'
 	target_size=(256,256)
-	normalize = transforms.Normalize(
-        mean=0.5,std=1
-    )
-	cam=normalize(cam)
-	cam_img=np.uint8(255*cam)
+	cam = cam - torch.min(cam)
+        cam_img = cam / torch.max(cam)
+        cam_img = np.uint8(255 * cam_img.detach())
 	cam_img=cv2.resize(cam_img,target_size)
 	return cam_img
 	
@@ -45,7 +43,7 @@ def get_cam(net,features,img,classes,root_img):
 	line='{:.3f}->{}'.format(prob[0],classes[index[0].item()])  #tensor can't be key of dict
 	print(line)
 	
-	weight=list(net.parameters())[-2][class_index]
+	weight=list(net.parameters())[-2][index[0].item()]
 	cam_img=returncam(weight,features[0])
 	
 	img=cv2.imread(root_img)
